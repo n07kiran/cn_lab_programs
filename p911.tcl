fb442e52fdd77e6b7d0aca5dd13ad931fa7c2109
n07kiran <n07kiran@gmail.com>
2025-03-25 22:56:08 +0530

    p9 updated, hopefully correct

---
+++ p911.tcl
@@ -0,0 +1,49 @@
+set ns [new Simulator]
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+
+set nf [open gbn.nam w]
+$ns namtrace-all $nf
+set f [open gbn.tr w]
+$ns trace-all $f
+
+$ns duplex-link $n0 $n2 1Mb 10ms DropTail
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+$ns duplex-link $n2 $n3 1Mb 10ms DropTail
+$ns duplex-link $n3 $n4 1Mb 10ms DropTail
+$ns duplex-link $n3 $n5 1Mb 10ms DropTail
+
+Agent/TCP set_nam_tracevar_true
+set tcp [new Agent/TCP]
+$tcp set fid 1
+$tcp set windowlnit 3
+$tcp set maxcwnd 3
+$ns attach-agent $n1 $tcp
+
+set sink [new Agent/TCPSink]
+$ns attach-agent $n4 $sink
+$ns connect $tcp $sink
+
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+
+# Start FTP traffic at t=0.05 seconds
+$ns at 0.05 "$ftp start"
+$ns at 0.25 "$ns queue-limit $n3 $n4 0"
+$ns at 0.35 "$ns queue-limit $n3 $n4 10"
+$ns at 1.0 "finish"
+
+proc finish {} {
+    global ns nf
+    $ns flush-trace
+    close $nf
+    exec nam gbn.nam &
+    exit 0
+}
+
+$ns run
