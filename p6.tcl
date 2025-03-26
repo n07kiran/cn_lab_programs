8250329979e4a72b4521480538ecfdf90ec874e8
n07kiran <n07kiran@gmail.com>
2025-03-26 05:55:32 +0530

    updated p6 , from udp to tcp

--- p6.tcl
+++ p6.tcl
@@ -18,19 +18,19 @@ set n5 [ $ns node ]
 
 $ns make-lan "$n0 $n1 $n2 $n3 $n4 $n5" 5Mb 10ms LL Queue/DropTail Mac/802_3
 
-set udp [ new Agent/UDP ]
-set null [ new Agent/Null ]
+set tcp [ new Agent/TCP ]
+set sink [ new Agent/TCPSink ]
 
-$ns attach-agent $n1 $udp
-$ns attach-agent $n5 $null
+$ns attach-agent $n1 $tcp
+$ns attach-agent $n5 $sink
 
-$ns connect $udp $null
+$ns connect $tcp $sink
 
-set cbr [ new Application/Traffic/CBR ]
-$cbr attach-agent $udp
+set ftp [ new Application/FTP ]
+$ftp attach-agent $tcp
 
-#$tcp set fid_ 1
-#$sink set fid_ 2
+$tcp set fid_ 1
+$sink set fid_ 2
 
 proc finish {} {
 	global ns tracefile namfile
@@ -41,6 +41,6 @@ proc finish {} {
         exit 0
 }
 
-$ns at 0.0 "$cbr start"
+$ns at 0.0 "$ftp start"
 $ns at 10.0 "finish"
 $ns run
